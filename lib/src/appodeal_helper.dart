3937fc4e070600b6485c45b459144545ac2d8932
Vursin <contact@softs.dev>
2022-06-26 13:56:26 +0700

    Update some useful method

--- lib/src/appodeal_helper.dart
+++ lib/src/appodeal_helper.dart
@@ -26,21 +26,21 @@ enum AppodealType {
 class AppodealHelper {
   AppodealHelper._();
 
+  /// Return true if ads are allowed and false otherwise.
   static bool isAllowedAds = false;
 
-  static String _appodealKey = '';
-
-  static List<AppodealType> _appodealTypes = [];
-
-  static bool _isTesting = true;
-
-  static bool _debugLog = false;
-
+  /// Return true if current platform is Android or iOS and false otherwise.
   static final isSupportedPlatform =
       UniversalPlatform.isAndroid || UniversalPlatform.isIOS;
 
+  /// Internal variables
+  static String _appodealKey = '';
+  static List<AppodealType> _appodealTypes = [];
+  static bool _isTesting = true;
+  static bool _debugLog = false;
   static bool _isInitialed = false;
 
+  /// Initial ConsentManager and Appodeal plugin.
   static Future<void> initial({
     required bool isTesting,
     required String keyAndroid,
@@ -90,95 +90,31 @@ class AppodealHelper {
     );
   }
 
-  static void dispose() async {
+  /// Destroy all Appodeal Ads. Default is to destroy all Appodeal ads.
+  static void dispose([AppodealType type = AppodealType.all]) async {
     // Không triển khai ad ở ngoài 2 platform này hoặc không hỗ trợ Ads
     if (!isSupportedPlatform || !isAllowedAds) return;
 
-    await Future.wait(
-      [for (final type in _appodealTypes) Appodeal.destroy(type.toAppodeal)],
-    );
+    Appodeal.destroy(type.toAppodeal);
   }
 
-  static Future<bool> _getConsent() async {
-    // Không triển khai ad ở ngoài 2 platform này
-    if (!isSupportedPlatform) return false;
-
-    ConsentManager.setConsentInfoUpdateListener(
-      (onConsentInfoUpdated, consent) => {_printDebug(consent)},
-      (onFailedToUpdateConsentInfo, error) => {_printDebug(error)},
-    );
-    await ConsentManager.requestConsentInfoUpdate(_appodealKey);
-
-    if ((await ConsentManager.shouldShowConsentDialog()) == ShouldShow.TRUE) {
-      ConsentManager.setConsentFormListener(
-        (onConsentFormLoaded) => {_printDebug(onConsentFormLoaded)},
-        (onConsentFormError, error) => {_printDebug(error)},
-        (onConsentFormOpened) => _printDebug(onConsentFormOpened),
-        (onConsentFormClosed, consent) => {_printDebug(consent)},
-      );
-
-      await ConsentManager.loadConsentForm();
-
-      _printDebug(
-        'is consent form loaded: ${await ConsentManager.consentFormIsLoaded()}',
-      );
-      if (await ConsentManager.consentFormIsLoaded()) {
-        await ConsentManager.showAsDialogConsentForm();
-        // ConsentManager.showAsActivityConsentForm();
-      }
-    }
-
-    final consentStatus = await ConsentManager.getConsentStatus();
-    final hasConsent = consentStatus == Status.PERSONALIZED ||
-        consentStatus == Status.PARTLY_PERSONALIZED;
-
-    return hasConsent;
-  }
-
-  /// Kiểm tra phiên bản cũ trên máy, nếu khác với phiên bản app đang chạy
-  /// thì sẽ không hiện Ads (tránh tình trạng bot của Google click nhầm).
-  /// Sẽ đếm số lần mở app, nếu đủ 3 lần sẽ cho phép mở Ads lại.
-  static Future<bool> _checkAllowedAds({
-    required CheckAllowAdsOption checkAllowAdsOption,
-  }) async {
-    if (checkAllowAdsOption.prefVersion != checkAllowAdsOption.appVersion) {
-      checkAllowAdsOption.writePref(checkAllowAdsOption.appVersion, 1);
-
-      _printDebug(
-        'Pref config không hiện Ads cho phiên bản này: $checkAllowAdsOption',
-      );
-
-      return false;
-    }
-
-    if (checkAllowAdsOption.count >= checkAllowAdsOption.allowAfterCount) {
-      // Nếu cloud không cho hiện Ads thì không cho hiện Ads nhưng những bước
-      // còn lại vẫn phải thực hiện.
-      if (!checkAllowAdsOption.cloudAllowed) {
-        _printDebug('Firebase remote config không hiện Ads cho phiên bản này');
-        return false;
-      }
-
-      return true;
-    }
-
-    checkAllowAdsOption.writePref(
-      checkAllowAdsOption.appVersion,
-      checkAllowAdsOption.count + 1,
-    );
-
-    _printDebug(
-      'Pref config không hiện Ads cho phiên bản này: $checkAllowAdsOption',
-    );
-
-    return false;
-  }
-
+  /// Get banner Widget
   static Widget get bannerWidget => const _BannerAd();
+
+  /// Get MREC Widget
   static Widget get mrecWidget => const _MrecAd();
 
-  // ignore: avoid_print
-  static _printDebug(Object? object) => _debugLog ? print(object) : null;
+  /// Hide specific ad
+  static Future<void> hideAd(AppodealType type) async {
+    return Appodeal.hide(type.toAppodeal);
+  }
+
+  /// Show specific ad
+  ///
+  /// Returns true if ad can be shown with this placement, otherwise false.
+  static Future<bool> showAd(AppodealType type) async {
+    return Appodeal.show(type.toAppodeal);
+  }
 }
 
 class CheckAllowAdsOption {
@@ -242,3 +178,82 @@ class _MrecAd extends StatelessWidget {
         : const SizedBox.shrink();
   }
 }
+
+/// Kiểm tra phiên bản cũ trên máy, nếu khác với phiên bản app đang chạy
+/// thì sẽ không hiện Ads (tránh tình trạng bot của Google click nhầm).
+/// Sẽ đếm số lần mở app, nếu đủ 3 lần sẽ cho phép mở Ads lại.
+Future<bool> _checkAllowedAds({
+  required CheckAllowAdsOption checkAllowAdsOption,
+}) async {
+  if (checkAllowAdsOption.prefVersion != checkAllowAdsOption.appVersion) {
+    checkAllowAdsOption.writePref(checkAllowAdsOption.appVersion, 1);
+
+    _printDebug(
+      'Pref config không hiện Ads cho phiên bản này: $checkAllowAdsOption',
+    );
+
+    return false;
+  }
+
+  if (checkAllowAdsOption.count >= checkAllowAdsOption.allowAfterCount) {
+    // Nếu cloud không cho hiện Ads thì không cho hiện Ads nhưng những bước
+    // còn lại vẫn phải thực hiện.
+    if (!checkAllowAdsOption.cloudAllowed) {
+      _printDebug('Firebase remote config không hiện Ads cho phiên bản này');
+      return false;
+    }
+
+    return true;
+  }
+
+  checkAllowAdsOption.writePref(
+    checkAllowAdsOption.appVersion,
+    checkAllowAdsOption.count + 1,
+  );
+
+  _printDebug(
+    'Pref config không hiện Ads cho phiên bản này: $checkAllowAdsOption',
+  );
+
+  return false;
+}
+
+Future<bool> _getConsent() async {
+  // Không triển khai ad ở ngoài 2 platform này
+  if (!AppodealHelper.isSupportedPlatform) return false;
+
+  ConsentManager.setConsentInfoUpdateListener(
+    (onConsentInfoUpdated, consent) => {_printDebug(consent)},
+    (onFailedToUpdateConsentInfo, error) => {_printDebug(error)},
+  );
+  await ConsentManager.requestConsentInfoUpdate(AppodealHelper._appodealKey);
+
+  if ((await ConsentManager.shouldShowConsentDialog()) == ShouldShow.TRUE) {
+    ConsentManager.setConsentFormListener(
+      (onConsentFormLoaded) => {_printDebug(onConsentFormLoaded)},
+      (onConsentFormError, error) => {_printDebug(error)},
+      (onConsentFormOpened) => _printDebug(onConsentFormOpened),
+      (onConsentFormClosed, consent) => {_printDebug(consent)},
+    );
+
+    await ConsentManager.loadConsentForm();
+
+    _printDebug(
+      'is consent form loaded: ${await ConsentManager.consentFormIsLoaded()}',
+    );
+    if (await ConsentManager.consentFormIsLoaded()) {
+      await ConsentManager.showAsDialogConsentForm();
+      // ConsentManager.showAsActivityConsentForm();
+    }
+  }
+
+  final consentStatus = await ConsentManager.getConsentStatus();
+  final hasConsent = consentStatus == Status.PERSONALIZED ||
+      consentStatus == Status.PARTLY_PERSONALIZED;
+
+  return hasConsent;
+}
+
+_printDebug(Object? object) =>
+    // ignore: avoid_print
+    AppodealHelper._debugLog ? print('[Appodeal Helper]: $object') : null;
